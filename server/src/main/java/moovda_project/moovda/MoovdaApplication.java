e87e62b397456c00683715e2918e305007534756
yeonwoo <ppupy1209@naver.com>
2023-07-03 16:47:27 +0900

    [init]: server 초기 세팅

---
+++ server/src/main/java/moovda_project/moovda/MoovdaApplication.java
@@ -0,0 +1,13 @@
+package moovda_project.moovda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoovdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoovdaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/moovda_project/moovda/MoovdaApplicationTests.java
@@ -0,0 +1,13 @@
+package moovda_project.moovda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoovdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
