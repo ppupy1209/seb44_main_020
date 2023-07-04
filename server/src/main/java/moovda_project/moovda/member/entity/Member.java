51efaae00a732149c6cee00c9de51d0a9dac258b
feebuno <wjdalsdn1117@gmail.com>
2023-07-04 17:15:13 +0900

    [feat] 멤버 엔티티 작성

---
+++ server/src/main/java/moovda_project/moovda/member/entity/Member.java
@@ -0,0 +1,37 @@
+package moovda_project.moovda.member.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member-id")
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String towatch;
+
+    @Column(nullable = false)
+    private  String watched;
+    @Column(nullable = false, updatable = false,unique = true)
+    private String email;
+
+//    @OneToMany(mappedBy = "member")
+//    private List<Question> questionList = new ArrayList<>();
+}
