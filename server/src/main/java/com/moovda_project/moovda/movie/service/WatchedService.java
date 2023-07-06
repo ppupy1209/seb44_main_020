d540e9db1be988fb75e6a6ecd08cf447089ba7e2
yeonwoo <ppupy1209@naver.com>
2023-07-06 19:57:00 +0900

    [feat] WatchedService 추가

---
+++ server/src/main/java/com/moovda_project/moovda/movie/service/WatchedService.java
@@ -0,0 +1,38 @@
+package com.moovda_project.moovda.movie.service;
+
+import com.moovda_project.moovda.exception.BusinessLogicException;
+import com.moovda_project.moovda.exception.ExceptionCode;
+import com.moovda_project.moovda.movie.entity.Watched;
+import com.moovda_project.moovda.movie.repository.WatchedRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class WatchedService {
+
+    private final WatchedRepository watchedRepository;
+
+    public Watched createWatched(Watched watched) {
+      return watchedRepository.save(watched);
+    }
+
+    public void deleteWatched(long watchedId) {
+         Watched watched = findVerifiedWatched(watchedId);
+
+         watchedRepository.delete(watched);
+    }
+
+    private Watched findVerifiedWatched(long watchedId) {
+        Optional<Watched> watched = watchedRepository.findById(watchedId);
+
+        Watched findWatched = watched.orElseThrow(() -> new BusinessLogicException(ExceptionCode.WATCHED_NOT_FOUND));
+
+        return findWatched;
+    }
+
+}
