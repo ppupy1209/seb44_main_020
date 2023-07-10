d591c65799b64416f09293c7e84bfc242ce321db
yeakode <hiwrld1010@gmail.com>
2023-07-10 13:35:18 +0900

    [design] AnswerBox 스타일 적용 (#21)

---
+++ client/src/components/Question/AnswerBox.styled.ts
@@ -0,0 +1,39 @@
+'use client';
+
+import styled from 'styled-components';
+
+export const AnswerBox = styled.div`
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 12px;
+  margin: 1rem;
+  padding: 0.75rem;
+`;
+
+export const BoxTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 0.5px solid white;
+`;
+
+export const LeftBox = styled.div`
+  display: flex;
+  gap: 8px;
+  margin-bottom: 2px;
+`;
+
+export const RightBox = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const AnswerBoxMid = styled.div``;
+
+export const ContentBox = styled.div``;
+
+export const BoxBottom = styled.div``;
+
+export const SelectedMovieBox = styled.div`
+  display: flex;
+`;
+
+export const MovieInfo = styled.div``;
