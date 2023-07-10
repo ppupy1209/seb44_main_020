876f2c80b7b9b69a3a89ebe3ca9dfb908afa02e4
yeakode <hiwrld1010@gmail.com>
2023-07-10 23:59:52 +0900

    [design] question-detail-page 디자인 (#25)

---
+++ client/src/app/questions/[questionId]/page.styled.ts
@@ -0,0 +1,112 @@
+'use client';
+
+import styled from 'styled-components';
+
+export const PageGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const AskBox = styled.div`
+  width: 1024px;
+  background-color: rgba(255, 255, 255, 0.2);
+  padding: 1rem;
+  margin: 0.75rem 1rem;
+  border: 0.5px solid #8a97ab;
+  border-radius: 12px;
+`;
+
+export const BoxTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const LeftBox = styled.div`
+  display: flex;
+  gap: 8px;
+  margin-left: 8px;
+`;
+
+export const Nickname = styled.p`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  color: white;
+  font-size: 0.75rem;
+`;
+
+export const Time = styled.p`
+  color: #8a97ab;
+  font-size: 0.75rem;
+  display: flex;
+  align-items: center;
+`;
+
+export const RightBox = styled.div`
+  display: flex;
+  gap: 8px;
+  margin: 0px 8px 8px 0px;
+`;
+
+export const EditBtn = styled.button`
+  width: 28px;
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0);
+  border-color: rgba(255, 255, 255, 0);
+  color: white;
+  font-size: 0.75rem;
+`;
+
+export const DeleteBtn = styled.button`
+  width: 28px;
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0);
+  border-color: rgba(255, 255, 255, 0);
+  color: white;
+  font-size: 0.75rem;
+`;
+
+export const BoxTitle = styled.h2`
+  font-weight: bold;
+  margin: 0px 0px 8px 8px;
+`;
+
+export const BoxMid = styled.div`
+  border-top: 0.1px solid #8a97ab;
+`;
+
+export const ContentBox = styled.div`
+  margin: 12px 8px 0px 8px;
+`;
+
+export const PageBottom = styled.div`
+  padding: 0.5rem;
+  margin: 0.75rem 1rem;
+  width: 1024px;
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 12px;
+  padding: 1rem;
+`;
+
+export const AnswerCreateBox = styled.div`
+  height: 310px;
+`;
+
+export const ButtonBox = styled.div`
+  display: flex;
+  justify-content: end;
+`;
+
+export const Button = styled.button`
+  padding: 12px 24px;
+  border-radius: 8px;
+  width: 100px;
+  background-color: #111321;
+  &:hover {
+    background-color: rgba(108, 99, 255, 0.3);
+  }
+  cursor: pointer;
+  color: white;
+`;
