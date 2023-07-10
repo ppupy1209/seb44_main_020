0cb52ea8a7f0219a9220a57452491d13c99cb1a8
yeakode <hiwrld1010@gmail.com>
2023-07-10 14:44:12 +0900

    [design] AnswerBox css 수정 (#21)

--- client/src/components/Question/AnswerBox.styled.ts
+++ client/src/components/Question/AnswerBox.styled.ts
@@ -6,34 +6,94 @@ export const AnswerBox = styled.div`
   background-color: rgba(255, 255, 255, 0.2);
   border-radius: 12px;
   margin: 1rem;
-  padding: 0.75rem;
+  padding: 1rem;
 `;
 
 export const BoxTop = styled.div`
   display: flex;
   justify-content: space-between;
-  border-bottom: 0.5px solid white;
+  border-bottom: 0.5px solid #8a97ab;
 `;
 
 export const LeftBox = styled.div`
   display: flex;
   gap: 8px;
-  margin-bottom: 2px;
+  margin: 0px 0px 8px 8px;
+`;
+
+export const Nickname = styled.p`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  color: white;
+  font-size: small;
+`;
+
+export const Time = styled.p`
+  color: #8a97ab;
+  font-size: small;
+  display: flex;
+  align-items: center;
 `;
 
 export const RightBox = styled.div`
   display: flex;
   gap: 8px;
+  margin: 0px 8px 8px 0px;
 `;
 
-export const AnswerBoxMid = styled.div``;
+export const EditBtn = styled.button`
+  width: 28px;
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0);
+  border-color: rgba(255, 255, 255, 0);
+  color: white;
+  font-size: small;
+`;
+
+export const DeleteBtn = styled.button`
+  width: 28px;
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0);
+  border-color: rgba(255, 255, 255, 0);
+  color: white;
+  font-size: small;
+`;
+
+export const AnswerBoxMid = styled.div`
+  margin: 12px 8px 0px 8px;
+`;
 
 export const ContentBox = styled.div``;
 
-export const BoxBottom = styled.div``;
+export const BoxBottom = styled.div`
+  margin: 16px 8px 0px 8px;
+`;
 
 export const SelectedMovieBox = styled.div`
   display: flex;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 8px;
+  width: 65%;
+  padding: 0.5rem;
+  gap: 12px;
+  cursor: pointer;
 `;
 
-export const MovieInfo = styled.div``;
+export const Poster = styled.div``;
+
+export const MovieInfo = styled.div`
+  font-size: small;
+  margin-top: 2px;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const MovieTitle = styled.p`
+  font-weight: bold;
+`;
+
+export const MovieReleaseDate = styled.p`
+  color: #1f305f;
+`;
