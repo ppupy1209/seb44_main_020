c0a3d9edc3d252e7cc15a2aa09d1f49b746765d3
minseong3 <alstjd1021@naver.com>
2023-07-12 15:58:54 +0900

    [refactor] Question 작성한 회원만 수정/삭제하는 기능 추가

--- server/src/main/java/com/moovda_project/moovda/module/question/service/QuestionService.java
+++ server/src/main/java/com/moovda_project/moovda/module/question/service/QuestionService.java
@@ -2,6 +2,8 @@ package com.moovda_project.moovda.module.question.service;
 
 import com.moovda_project.moovda.global.exception.BusinessLogicException;
 import com.moovda_project.moovda.global.exception.ExceptionCode;
+import com.moovda_project.moovda.module.member.entity.Member;
+import com.moovda_project.moovda.module.member.service.MemberService;
 import com.moovda_project.moovda.module.question.entity.Question;
 import com.moovda_project.moovda.module.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,23 +22,28 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
+    private final MemberService memberService;
+
     /** 질문 등록 **/
     /** ? : 멤버id를 받아서**/
     // TODO : 작성한 회원이 존재하는 회원인지 확인
+
     public Question createQuestion(Question question) {
+        memberService.findVerifiedMember(question.getMember().getMemberId());
         return questionRepository.save(question);
     }
 
     /** 질문 수정 **/
-    public Question updateQuestion(Question question) {
-        Question findQuestion = findVerifiedQuestion(question.getQuestionId());
+    public Question updateQuestion(Question question, long authenticationMemberId) {
+        Question findQuestionId = findVerifiedQuestion(question.getQuestionId());
         // TODO : 작성한 회원만 수정 가능
 
-        Optional.ofNullable(question.getContent()).ifPresent(content -> findQuestion.setContent(content));
-        Optional.ofNullable(question.getTitle()).ifPresent(title -> findQuestion.setTitle(title));
+        checkValidatedMember(authenticationMemberId,findQuestionId);
 
+        findQuestionId.setTitle(question.getTitle());
+        findQuestionId.setContent(question.getContent());
 
-        return questionRepository.save(findQuestion);
+        return questionRepository.save(findQuestionId);
     }
 
     /** 질문 조회 **/
@@ -57,12 +64,14 @@ public class QuestionService {
     }
 
     /** 질문 삭제 **/
-    public void deleteQuestion(long questionId) {
-        // TODO : token으로 로그인 한 회원 확인
-        // 로그인 회원의 id를 얻어서 question을 등록한 회원의 id와 비교
-        Question findQuestion = findVerifiedQuestion(questionId);
+    public void deleteQuestion(long questionId,long authenticationMemberId) {
 
-        questionRepository.delete(findQuestion);
+        // 로그인 회원의 id를 얻어서 question을 등록한 회원의 id와 비교
+        Question findQuestionId = findVerifiedQuestion(questionId);
+
+        checkValidatedMember(authenticationMemberId,findQuestionId);
+
+        questionRepository.delete(findQuestionId);
 
     }
     /** 질문이 등록된 질문인지 확인 메서드 **/
@@ -74,5 +83,12 @@ public class QuestionService {
                 new BusinessLogicException(ExceptionCode.QUESTION_NOT_FOUND));
 
         return question;
+    }
+
+    /** 작성한 회원인지 확인하는 메서드 **/
+    private void checkValidatedMember(long memberId, Question question) {
+        if(question.getMember().getMemberId()!= memberId) {
+            throw new BusinessLogicException(ExceptionCode.MEMBER_UNAUTHORIZED);
+        }
     }
 }
