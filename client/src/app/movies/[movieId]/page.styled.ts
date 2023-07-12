0f1ba96046b65c1d37802bfb6da446941806ab10
hjthebunny <monicahyunji@gmail.com>
2023-07-12 15:47:01 +0900

    [feat] 영화 상세 - 정보 UI 구현

--- client/src/app/movies/[movieId]/page.styled.ts
+++ client/src/app/movies/[movieId]/page.styled.ts
@@ -3,12 +3,16 @@ import styled from 'styled-components';
 
 export const Wrapper=styled.div`
 margin: 50px 90px 0 90px;
-height: 100vh;
+/* height: 100vh; */
+/* border:1px solid white; */
 `;
+
 
 export const MovieInfoWrapper=styled.div`
 /* border: 1px solid red; */
-display:flex;`;
+display:flex;
+justify-content:center;
+gap:100px;`;
 
 export const PosterStar=styled.div`
 width: fit-content;
@@ -26,30 +30,92 @@ border-radius:20px;`
 export const MyStar=styled.div``;
 
 export const MovieInfo=styled.div`
-margin-left:50px;`;
+/* border: 1px solid white; */
+position: relative;
+`;
 
 export const Title=styled.h1`
 letter-spacing:2px;
+font-size:40px ;
 `;
 
 export const DetailWrapper=styled.div`
 margin-top: 40px;
 display:flex;
 flex-direction:column;
-gap:10px;`;
+gap:10px;
+`;
+
 export const DateAndCountry=styled.div``;
+
 export const Genre=styled.div``;
+
 export const RunningTime=styled.div``;
-export const StarAvr=styled.div``;
+
+export const StarAvg=styled.div`
+width:100px;
+height:100px;
+background-color:#9EA9D8;
+position:absolute;
+right:0;
+display:flex;
+gap:10px;
+flex-direction:column;
+justify-content:center;
+align-items:center;`;
+
+export const avNum=styled.div`
+font-size:30px;
+color: #01123D;
+font-weight:bold;`;
+
+export const avText=styled.div`
+color: #01123D;
+font-size: 14px;
+`;
+
 export const Summary=styled.div`
+border-top:1px solid #283A6D;
+padding-top: 40px;
 margin-top: 40px;
 width:600px;
 line-height: 1.5em;
 text-align: justify;
 `;
 
-export const ToWatchBtn=styled.button``;
-export const WatchedBtn=styled.button``;
+export const ToWatchBtn=styled.button`
+display: flex;
+gap: 10px;
+justify-content: center;
+align-items: center;
+height:40px;
+padding: 10px;
+width: 200px;
+background-color: #283A6D;
+border: 2px solid #5973B6;
+border-radius:5px;
+color: #ffffff;
+cursor: pointer;
+font-weight: 400;
+&:hover{
+    background-color:#5973B6 ;
+    border: 2px solid #283A6D; 
+}`;
 
+export const WatchedBtn=styled(ToWatchBtn)``;
 
+export const BtnWrapper=styled.div`
+margin-top: 50px;
+/* border:1px solid red; */
+display:flex;
+/* justify-content:center; */
+gap:20px;
+`;
 
+export const StaffListWrapper=styled.div`
+margin-top: 40px;
+border-top: 1px solid #466093;
+padding-top: 40px;
+height:500px;`;
+
+export const StaffList=styled.div``;
--- client/src/app/movies/[movieId]/page.tsx
+++ client/src/app/movies/[movieId]/page.tsx
@@ -1,5 +1,12 @@
 'use client'
 import * as S from './page.styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import Pen from '@/assets/penIcon.svg'
+import Plus from '@/assets/plus.svg'
+
+
 export default function MyPage() {
     return(
         <S.Wrapper>
@@ -9,6 +16,10 @@ export default function MyPage() {
                     <S.MyStar>별점</S.MyStar>
                 </S.PosterStar>
                 <S.MovieInfo>
+                    <S.StarAvg>
+                        <S.avNum>5.0</S.avNum>
+                        <S.avText>평균 별점</S.avText>
+                    </S.StarAvg>
                     <S.Title>엘리멘탈</S.Title>
                     <S.DetailWrapper>
                     <S.DateAndCountry>2023 • 미국</S.DateAndCountry>
@@ -16,15 +27,22 @@ export default function MyPage() {
                     <S.Genre>애니메이션 / 모험 / 판타지 / SF / 로맨스</S.Genre>
                     <S.RunningTime>93분</S.RunningTime>
                     </S.DetailWrapper>
+                    <S.BtnWrapper>
+                        <S.ToWatchBtn><span><Pen fill="#ffffff"/></span><span>별점・코멘트</span> </S.ToWatchBtn>
+                        <S.WatchedBtn><span><Plus fill="#ffffff"/></span><span>보고싶어요</span></S.WatchedBtn>
+                    </S.BtnWrapper>
+
                     <S.Summary>
                         불, 물, 공기, 흙 4개의 원소들이 살고 있는 ‘엘리멘트 시티’
                         재치 있고 불처럼 열정 넘치는 ‘앰버'는 어느 날 우연히
                         유쾌하고 감성적이며 물 흐르듯 사는 '웨이드'를 만나 특별한 우정을 쌓으며,
-                        지금껏 믿어온 모든 것들이 흔들리는 새로운 경험을 하게 되는데...</S.Summary>
-
-
+                        지금껏 믿어온 모든 것들이 흔들리는 새로운 경험을 하게 되는데...
+                    </S.Summary>
                 </S.MovieInfo>
             </S.MovieInfoWrapper>
+            <S.SectionWrapper>
+                <S.StaffList></S.StaffList>
+            </S.SectionWrapper>
         </S.Wrapper>
     )
 }
