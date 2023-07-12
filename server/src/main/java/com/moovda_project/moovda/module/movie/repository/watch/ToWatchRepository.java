aeb30c5b7574dd47dfe74e69c56351daf5583a3b
yeonwoo <ppupy1209@naver.com>
2023-07-12 23:17:32 +0900

    [feat] ToWatchRepository 메서드 추가

--- server/src/main/java/com/moovda_project/moovda/module/movie/repository/watch/ToWatchRepository.java
+++ server/src/main/java/com/moovda_project/moovda/module/movie/repository/watch/ToWatchRepository.java
@@ -1,7 +1,14 @@
 package com.moovda_project.moovda.module.movie.repository.watch;
 
+import com.moovda_project.moovda.module.member.entity.Member;
+import com.moovda_project.moovda.module.movie.entity.Movie;
 import com.moovda_project.moovda.module.movie.entity.watch.ToWatch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ToWatchRepository extends JpaRepository<ToWatch, Long> {
+    Optional<ToWatch> findByMemberAndMovie(Member member, Movie movie);
+
+    void deleteByMemberAndMovie(Member member, Movie movie);
 }
