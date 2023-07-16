76b18ae88591807e253177f122fd8c5a6c7d9553
yeonwoo <ppupy1209@naver.com>
2023-07-16 17:40:16 +0900

    [refactor] Like 리팩토링 및 주석 추가

--- server/src/main/java/com/moovda_project/moovda/module/like/service/LikeService.java
+++ server/src/main/java/com/moovda_project/moovda/module/like/service/LikeService.java
@@ -3,7 +3,6 @@ package com.moovda_project.moovda.module.like.service;
 import com.moovda_project.moovda.module.comment.entity.Comment;
 import com.moovda_project.moovda.module.comment.service.CommentService;
 import com.moovda_project.moovda.module.like.entity.Like;
-import com.moovda_project.moovda.module.comment.repository.CommentRepository;
 import com.moovda_project.moovda.module.like.repository.LikeRepository;
 import com.moovda_project.moovda.module.member.entity.Member;
 import com.moovda_project.moovda.module.member.service.MemberService;
@@ -18,29 +17,24 @@ public class LikeService {
     private final LikeRepository likeRepository;
     private final CommentService commentService;
     private final MemberService memberService;
-    private final CommentRepository commentRepository;
 
     public void addLike(long memberId, long commentId) {
 
         Comment comment = commentService.findVerifiedComment(commentId);
         Member member = memberService.findVerifiedMember(memberId);
+
         Like like = new Like(member,comment);
 
-        if(isNotAlreadyLike(member,comment)) {
-            comment.addLikes(like);
-
-            commentRepository.save(comment);
-
+        // 좋아요 목록에 있는지 체크
+        if(isNotAlreadyLike(member,comment)) {   // 없으면 좋아요 추가
             likeRepository.save(like);
+
         }
-        else {
-            comment.removeLikes(like);
-
-            commentRepository.save(comment);
-
+        else {  // 있으면 좋아요 삭제
             likeRepository.deleteByMemberAndComment(member, comment);
         }
     }
+
     private boolean isNotAlreadyLike(Member member, Comment comment) {
         return likeRepository.findByMemberAndComment(member,comment).isEmpty();
     }
