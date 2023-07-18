ec1be12a9a7e7f396b593c38eaa2829cb464a3a9
yeonwoo <ppupy1209@naver.com>
2023-07-18 13:34:00 +0900

    [feat] HealthController 추가

---
+++ server/src/main/java/com/moovda_project/moovda/module/HealthController.java
@@ -0,0 +1,26 @@
+package com.moovda_project.moovda.module;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController("/test")
+public class HealthController
+{
+    public ResponseEntity<String> healthCheck() {
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            String hostname = ip.getHostName();
+            String ipAddress = ip.getHostAddress();
+
+            String response = "EC2 Hostname: " + hostname + "\nEC2 IP Address: " + ipAddress;
+            return ResponseEntity.ok(response);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error getting hostname and IP address.");
+        }
+    }
+}
