538ccf1110ff9277b8633e9411f35e36291f0003
minseong3 <alstjd1021@naver.com>
2023-07-20 17:25:31 +0900

    [feat] questionToQuestionResponseDto 페이지네이션 추가

--- server/src/main/java/com/moovda_project/moovda/module/question/mapper/QuestionMapper.java
+++ server/src/main/java/com/moovda_project/moovda/module/question/mapper/QuestionMapper.java
@@ -1,6 +1,8 @@
 package com.moovda_project.moovda.module.question.mapper;
 
+import com.moovda_project.moovda.global.dto.PageDto;
 import com.moovda_project.moovda.module.answer.dto.AnswerDto;
+import com.moovda_project.moovda.module.answer.entity.Answer;
 import com.moovda_project.moovda.module.question.dto.QuestionDto;
 import com.moovda_project.moovda.module.question.entity.Question;
 import org.mapstruct.Mapper;
@@ -15,9 +17,21 @@ public interface QuestionMapper {
     @Mapping(source = "memberId", target = "member.memberId")
     Question questionPostDtoToQuestion(QuestionDto.Post questionPostDto);
     Question questionPatchDtoToQuestion(QuestionDto.Patch questionPatchDto);
-    List<QuestionDto.Response> questionsToQuestionResponseDtos(List<Question> questions);
-    default QuestionDto.Response questionToQuestionResponseDto(Question question) {
 
+    default List<QuestionDto.Response> questionsToQuestionResponseDtos(List<Question> questions) {
+        return questions.stream()
+                .map(question -> QuestionDto.Response.builder()
+                        .questionId(question.getQuestionId())
+                        .title(question.getTitle())
+        //                .nickname(question.getMember().getNickname())
+                        .createdAt(question.getCreatedAt())
+                        .answerCount(question.getAnswerCount())
+                        .views(question.getViews())
+                        .build())
+                .collect(Collectors.toList());
+    }
+    default QuestionDto.Response questionToQuestionResponseDto(Question question, int page, int pageSize) {
+/** TODO : pageInfo 출력 해야함  **/
         QuestionDto.Response questionResponseDto =
                 QuestionDto.Response.builder()
                         .questionId(question.getQuestionId())
@@ -31,8 +45,23 @@ public interface QuestionMapper {
                         .build();
 
         if(question.getAnswers() != null) {
+            List<Answer> answers = question.getAnswers();
+            int totalAnswers = answers.size();
+
+            // 시작 인덱스 계산
+            int startIndex = (page-1) * pageSize;
+            // 끝 인덱스 계산
+            int endIndex = Math.min(startIndex + pageSize, totalAnswers);
+            List<Answer> paginatedAnswers = answers.subList(startIndex, endIndex);
+
+            PageDto pageInfo = PageDto.builder()
+                    .currentPage(page)
+                    .total(totalAnswers)
+                    .pageSize(pageSize)
+                    .build();
+
             List<AnswerDto.Response> answerResponseDtos =
-                    question.getAnswers()
+                    paginatedAnswers
                             .stream()
                             .map(answer -> AnswerDto.Response.builder()
                                     .answerId(answer.getAnswerId())
