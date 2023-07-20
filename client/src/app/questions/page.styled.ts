e69c65976d927515237a60ab039d8d8862b50ea0
yeakode <hiwrld1010@gmail.com>
2023-07-20 12:17:02 +0900

    [style] question-list-page css (#13)

--- client/src/app/questions/page.styled.ts
+++ client/src/app/questions/page.styled.ts
@@ -45,3 +45,32 @@ export const Button = styled.button`
 export const QuestionBox = styled.div`
   width: 1024px;
 `;
+
+export const PaginationBtnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const PaginationBtnBox = styled.div`
+  display: inline;
+`;
+
+export const PaginationBtn = styled.button`
+  background-color: rgba(66, 99, 235, 0.5);
+  border: solid 1px #616c7f;
+  color: white;
+  padding: 4px;
+  border-radius: 4px;
+  width: 30px;
+  height: 32px;
+  margin: 32px 4px;
+  cursor: pointer;
+  &:hover {
+    color: #111321;
+    background-color: rgba(255, 255, 255, 0.9);
+  }
+  &.active {
+    color: #111321;
+    background-color: rgba(255, 255, 255, 0.9);
+  }
+`;
