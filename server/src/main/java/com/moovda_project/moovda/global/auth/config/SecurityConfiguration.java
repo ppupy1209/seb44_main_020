1b8c52a841025d9b103f10ea2cd67dc05a117ac8
feebuno <wjdalsdn1117@gmail.com>
2023-07-22 15:46:27 +0900

    [feat] cors 설정 변경

--- server/src/main/java/com/moovda_project/moovda/global/auth/config/SecurityConfiguration.java
+++ server/src/main/java/com/moovda_project/moovda/global/auth/config/SecurityConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,6 +25,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.*;
+
 
 @Configuration
 public class SecurityConfiguration {
@@ -73,6 +75,8 @@ public class SecurityConfiguration {
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));  // (8-2)
         configuration.setAllowedHeaders(Arrays.asList("*")); // CORS 정책 추가 (1)
         configuration.setExposedHeaders(Arrays.asList("*")); // CORS 정책 추가 (2)
+
+        configuration.addAllowedOrigin("https://moovda.com");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();   // (8-3)
         source.registerCorsConfiguration("/**", configuration);      // (8-4)     주의 사항: 콘텐츠 표시 오류로 인해 '/**'를 '\/**'로 표기했으니 실제 코드 구현 시에는 '\(역슬래시)'를 빼 주세요.
