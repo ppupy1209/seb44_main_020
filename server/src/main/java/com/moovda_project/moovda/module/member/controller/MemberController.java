5eaf52ca43ec02fe7734304821ef3452c8f70405
feebuno <wjdalsdn1117@gmail.com>
2023-07-23 14:27:15 +0900

    [feat] oauth 설정

--- server/src/main/java/com/moovda_project/moovda/module/member/controller/MemberController.java
+++ server/src/main/java/com/moovda_project/moovda/module/member/controller/MemberController.java
@@ -31,15 +31,15 @@ public class MemberController {
         this.mapper = mapper;
     }
 
-    @PostMapping
-    public ResponseEntity postMember(@Valid @RequestBody MemberDto.Post requestBody) {
-        Member member = mapper.memberPostToMember(requestBody);
-
-        Member createdMember = memberService.createMember(member);
-        URI location = UriCreator.createUri(MEMBER_DEFAULT_URL, createdMember.getMemberId());
-
-        return ResponseEntity.created(location).build();
-    }
+//    @PostMapping
+//    public ResponseEntity postMember(@Valid @RequestBody MemberDto.Post requestBody) {
+//        Member member = mapper.memberPostToMember(requestBody);
+//
+//        Member createdMember = memberService.createMember(member);
+//        URI location = UriCreator.createUri(MEMBER_DEFAULT_URL, createdMember.getMemberId());
+//
+//        return ResponseEntity.created(location).build();
+//    }
 
     @GetMapping("/{member_id}")
     public ResponseEntity getMember(@PathVariable("member_id") @Positive long memberId) {
