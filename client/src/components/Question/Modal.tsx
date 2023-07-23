78046bd5f54bf09f55ac225fa2f7739d6a37cb83
yeakode <hiwrld1010@gmail.com>
2023-07-23 21:36:44 +0900

    [fix] 모달창 css, pagination type 수정 (#24)

--- client/src/components/Question/Modal.tsx
+++ client/src/components/Question/Modal.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import * as S from '@/components/Question/Modal.styled';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
@@ -12,6 +14,8 @@ interface ModalProps {
 
 const Modal = ({ onSelectItem }: ModalProps) => {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const isModalOpen = searchParams.get('modal') === 'true';
 
   const onSelectMovie = (movie: SearchMovieList) => {
     onSelectItem(movie);
@@ -20,12 +24,44 @@ const Modal = ({ onSelectItem }: ModalProps) => {
   return (
     <S.ModalWrapper>
       <S.ModalContent>
-        <SearchModal onSelect={onSelectMovie} />
-        <Link href={pathname} replace>
-          <button>완료하기</button>
-        </Link>
+        <div>
+          <Link href={pathname} replace>
+            <S.SubmitButton>
+              <S.ClosedIcon />
+            </S.SubmitButton>
+          </Link>
+        </div>
+        <S.SearchTipBanner>
+          <SearchTipContainer />
+        </S.SearchTipBanner>
+        <S.SearchInputAndSearchedList>
+          <SearchModal onSelect={onSelectMovie} />
+        </S.SearchInputAndSearchedList>
       </S.ModalContent>
     </S.ModalWrapper>
+  );
+};
+
+const SearchTipContainer = () => {
+  return (
+    <S.SearchTipWrapper>
+      {/* <S.ImageContainer>
+        <S.ImageBox>
+          <HeaderLogo />
+        </S.ImageBox>
+      </S.ImageContainer> */}
+      <div>
+        <S.SearchDiscription>
+          <S.SearchTip>
+            💡 영화 제목, 감독, 배우 모두 검색 가능합니다.
+          </S.SearchTip>
+          <p>💡 전체 키워드를 입력하면 더 빠른 결과를 얻을 수 있습니다.</p>
+          <S.SearchExample>
+            예: 해리 포터 ❌ ➡️ 해리 포터와 아즈카반의 죄수 ⭕️
+          </S.SearchExample>
+        </S.SearchDiscription>
+      </div>
+    </S.SearchTipWrapper>
   );
 };
 
@@ -33,6 +69,7 @@ export interface SearchMovieList {
   movieId: string;
   title: string;
   poster: string;
+  prodYear: string;
   pageInfo: {
     currentPage: number;
     pageSize: number;
@@ -55,18 +92,33 @@ const SearchModal = ({ onSelect }: SearchModalProps) => {
     SearchMovieList[] | undefined
   >(undefined);
   const [searchKeyword, setSearchKeyword] = useState<string>('');
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(1);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (searchKeyword) {
       const MovieSearchData = async () => {
-        const source = `${process.env.NEXT_PUBLIC_API_URL}/movies/search?page=1&keyword=${searchKeyword}`;
+        const source = `${process.env.NEXT_PUBLIC_API_URL}/movies/search?page=${currentPage}&keyword=${searchKeyword}`;
         const response = await axios.get(source, { headers: {} });
         // console.log(response.data);
         setSearchResult(response.data.movies);
+        setTotalPages(response.data.pageInfo.total);
         // console.log(searchResult);
       };
       MovieSearchData();
+    }
+  };
+
+  const onPrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  const onNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
     }
   };
 
@@ -82,14 +134,59 @@ const SearchModal = ({ onSelect }: SearchModalProps) => {
   return (
     <form onSubmit={onSubmit}>
       <S.SearchContainer>
-        <S.SearchInput type="text" onChange={onChange} />
-        <button type="submit">검색</button>
+        <S.SearchInput
+          type="text"
+          onChange={onChange}
+          placeholder="추천하고 싶은 영화를 검색해 보세요!"
+        ></S.SearchInput>
+        <S.SearchButton type="submit">
+          <S.SearchIcon>
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              style={{ color: 'grey', fontSize: '18px' }}
+            />
+          </S.SearchIcon>
+        </S.SearchButton>
       </S.SearchContainer>
       <SearchedMovieList
         searchResult={searchResult}
         onSelectMovie={onSelectMovie}
       />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPrevPage={onPrevPage}
+        onNextPage={onNextPage}
+      />
     </form>
+  );
+};
+
+export interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPrevPage: () => void;
+  onNextPage: () => void;
+}
+
+const Pagination = ({
+  currentPage,
+  totalPages,
+  onPrevPage,
+  onNextPage,
+}: PaginationProps) => {
+  return (
+    <div>
+      {typeof currentPage === 'number' && typeof totalPages === 'number' && (
+        <S.Fragment>
+          <S.PaginationButton onClick={onPrevPage}>◀️</S.PaginationButton>
+          <S.ShowPageContainer>
+            {currentPage} / {totalPages}
+          </S.ShowPageContainer>
+          <S.PaginationButton onClick={onNextPage}>▶️</S.PaginationButton>
+        </S.Fragment>
+      )}
+    </div>
   );
 };
 
@@ -124,10 +221,13 @@ const SearchedMovieList = ({
           <div>
             <S.SearchedMovieTitle>{movie.title}</S.SearchedMovieTitle>
           </div>
+          <div>
+            <S.SearchedMovieProdYear>{movie.prodYear}</S.SearchedMovieProdYear>
+          </div>
         </S.SearchedMovieBox>
       ))}
     </S.SearchedListBox>
-);
+  );
 };
 
 export default Modal;
